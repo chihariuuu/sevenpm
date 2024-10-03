f0869bd39385700eb190f5178dabe61064c03585
Princess Joy Masinas <masinaspj27@gmail.com>
2024-10-03 11:27:14 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	string nickname;
+	string cyl;
+	string school;
+	cout << "Nickname: ";
+	cin >> nickname;
+	cout << "Course-Year Level: ";
+	cin >> cyl;
+	cout << "School: ";
+	cin >> school;
+	return 0;
+}
