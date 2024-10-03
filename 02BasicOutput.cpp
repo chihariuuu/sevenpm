1a5e68bfb3d8d8249d8150ad36942a28eec56b49
Princess Joy Masinas <masinaspj27@gmail.com>
2024-10-03 10:57:01 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " Our Lady of Lourdes College Foundation " << endl;
+	cout << " College of Computer Studies " << endl;
+	cout << " BSIT 1B " << endl;
+	cout << " Princess Joy Masinas " << endl;
+	cout << " Purok 3, Brgy. Dogongan, Daet, Camarines Norte "<< endl;
+	return 0;
+	
+}
